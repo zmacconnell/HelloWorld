f4ba37d0b47a9e1cff99a929f1289e8aa39ffeef
Zach MacConnell <zachary.macconnell@gmail.com>
2024-01-10 22:08:56 -0700

    Removed unnecessary files

--- helloworld.js
+++
@@ -1,6 +0,0 @@
-function sayHello(who) {
-    return "Hello " + who;
-}
-console.log(sayHello("World"));
-// npx tsc helloworld.ts
-// node helloworld.js
--- helloworld.ts
+++
@@ -1,7 +0,0 @@
-function sayHello(who : string) : string {
-    return "Hello " + who;
-}
-
-console.log(sayHello("World"))
-
-// npx tsc helloworld.ts
